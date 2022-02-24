d84ffb41dbd3ebd2202d4911662ec7772ac4e8ba
Dominic Pisano <pisano.dominic@gmail.com>
2022-02-24 09:19:41 -0500

    fixed it to only accept a number 0-100 for grades

--- main.js
+++ main.js
@@ -5,27 +5,27 @@ var computeFinal = require("./computeFinal.js");
 var prompt = require('prompt');
  var schema = {
     properties: {      
-      HW1: { type: 'number'},
-      HW2: { type: 'number'},
-      HW3: { type: 'number'},
-      Exam1: { type: 'number'},
-      Exam2: { type: 'number'}
+      HW1: {pattern: /^[1-9][0-9]?$|^100$/,message: 'Grade must be number 0-100',required: true},
+      HW2: {pattern: /^[1-9][0-9]?$|^100$/,message: 'Grade must be number 0-100',required: true},
+      HW3: {pattern: /^[1-9][0-9]?$|^100$/,message: 'Grade must be number 0-100',required: true},
+      Exam1: {pattern: /^[1-9][0-9]?$|^100$/,message: 'Grade must be number 0-100',required: true},
+      Exam2: {pattern: /^[1-9][0-9]?$|^100$/,message: 'Grade must be number 0-100',required: true},
     },
   };
 
   prompt.get(schema, function (err, result) {
     //outputs all the grades from input
     console.log('Grades:');
-    console.log(' Homework 1:', result.HW1);
-    console.log(' Homework 2:', result.HW2);
-    console.log(' Homework 3:', result.HW3);
-    console.log(' Exam 1:', result.Exam1);
-    console.log(' Exam 2:', result.Exam2);
+    console.log(' Homework 1:', Number(result.HW1));
+    console.log(' Homework 2:', Number(result.HW2));
+    console.log(' Homework 3:', Number(result.HW3));
+    console.log(' Exam 1:', Number(result.Exam1));
+    console.log(' Exam 2:', Number(result.Exam2));
     
     //calls all the functions to get percentages of the homeworks, each exam, and the final grade
-    var totalHW = computeHW.computeHW(result.HW1,result.HW2,result.HW3);
-    var exam1 = computeExam.computeExam(result.Exam1)
-    var exam2 = computeExam.computeExam(result.Exam2)
+    var totalHW = computeHW.computeHW(Number(result.HW1),Number(result.HW2),Number(result.HW3));
+    var exam1 = computeExam.computeExam(Number(result.Exam1))
+    var exam2 = computeExam.computeExam(Number(result.Exam2))
     var finalGrade = computeFinal.computeFinal(totalHW,exam1,exam2);
 
     //proof that the math works, I don't know if we need to show this or just the letter grade
