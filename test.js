66f839d9ea307191c9b602476846d3870201eeba
abhinavballakur <ab896224@wcupa.edu>
2022-03-03 17:24:37 -0500

    test.js fully functional

--- test.js
+++ test.js
@@ -1,6 +1,10 @@
 
-// Compute Exam test Cases
+
 const computeExam = require('./computeExam.js');
+const computeHW = require('./computeHW.js');
+const computeFinal = require('./computeFinal.js');
+
+// Compute Exam test Cases
 describe("Compute Exam Test", () => {
     test('Input of 100 should return 40', () => {
       expect(computeExam.computeExam(Number(100))).toBe(40);
@@ -23,27 +27,44 @@ describe("Compute Exam Test", () => {
 
   //Compute HW test Cases
 
-  const computeHW = require('./computeHW.js');
+  
 describe("Compute HW Test", () => {
 
   test('Input of 100,100,100 should return 20', () => {
-    expect(computeHW.computeHW((100,100,100))).toBe(20);
+    
+    const result  = computeHW.computeHW(100,100,100);
+    expect(result).toBe(20);
   });
 
   test('Input of 100,90,80 should return 18', () => {
-    expect(computeHW.computeHW((100,90,80))).toBe(18);
+    
+    result  = computeHW.computeHW(100,90,80); 
+    expect(result).toBe(18);
   });
 
-  test('Input of 100,75,30 should return 13.6', () => {
-    expect(computeHW.computeHW((100,75,30))).toBe(13.6);
+  test('Input of 100,75,50 should return 15', () => {
+    
+    result  = computeHW.computeHW(100,75,50); 
+    expect(result).toBe(15);
   });
 
 })
 
-const computeFinal = require('./computeFinal.js');
+
+
 describe("Compute Final Test", () => {
 
-  test('Input of 100,100,100 should return A', () => {
-    expect(computeHW.computeHW((100,75,30))).toBe('A');
+  const result  = computeFinal.computeFinal(20,40,40);
+  test('Input of 20,40,40 should return A', () => {
+    expect(result).toBe('A');
+  });
+  
+  const result2  = computeFinal.computeFinal(18,24,40);
+  test('Input of 18,24,40 should return B-', () => {
+    expect(result2).toBe('B-');
+  });
+   const result3  = computeFinal.computeFinal(15,16,40);
+  test('Input of 15,16,40 should return C-', () => {
+    expect(result3).toBe('C-');
   });
 }) 
